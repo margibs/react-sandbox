b789a12d045f97e7f057346c47e5868e18c9b472
Rumar Gibs Ecaldre <margibs_hbb@yahoo.com>
2024-08-04 20:57:43 +0800

    Added more sections

---
+++ src/assets/index.ts
@@ -0,0 +1,29 @@
+import doctor from "./doctor.png";
+import covid19 from "./covid19.jpg";
+import heartLungs from "./heartLungs.jpg";
+import supplement from "./supplement.jpg";
+import mentalHealth from "./mentalHealth.jpg";
+import whyYouChooseUs from "./why-you-choose-us.png";
+import user1 from "./user1.png";
+import user2 from "./user2.png";
+import user3 from "./user3.png";
+import user4 from "./user4.png";
+import user5 from "./user5.png";
+import user6 from "./user6.png";
+import reviewStar from "./review-star.png";
+
+export {
+  doctor,
+  covid19,
+  heartLungs,
+  supplement,
+  mentalHealth,
+  whyYouChooseUs,
+  user1,
+  user2,
+  user3,
+  user4,
+  user5,
+  user6,
+  reviewStar,
+};
--- src/pages/esheba/Esheba.tsx
+++ src/pages/esheba/Esheba.tsx
@@ -1,80 +1,217 @@
-import doctor from "../../assets/doctor.png";
+import {
+  doctor,
+  covid19,
+  heartLungs,
+  supplement,
+  mentalHealth,
+  whyYouChooseUs,
+  user1,
+  user2,
+  user3,
+  user4,
+  user5,
+  user6,
+  reviewStar,
+} from "../../assets";
 
 const Esheba = () => {
   return (
-    <section className="bg-[#E1EEFF] ibm-plex">
-      {/* header */}
-      <div className="container mx-auto">
-        <header className="flex w-full items-center justify-between py-12">
-          <a href="#" className="text-black font-semibold">
-            E-<span className="text-[#0360D9]">sheba</span>
-          </a>
-          <nav className="flex space-x-2 justify-center">
-            <a href="#" className="hover:text-blue-500 hover:underline">
-              Home
+    <>
+      <section className="bg-[#E1EEFF] ibm-plex">
+        {/* header */}
+        <div className="w-[1440px] mx-auto">
+          <header className="flex w-full items-center justify-between py-12">
+            <a href="#" className="text-black font-semibold">
+              E-<span className="text-[#0360D9]">sheba</span>
             </a>
-            <a href="#">About</a>
-            <a href="#">Application</a>
-            <a href="#">History</a>
-          </nav>
-          <span className="flex space-x-2">
-            <button className="border border-blue-600 rounded-full p-2 bg-blue-500">
-              Log In
-            </button>
-            <button>Sign Up</button>
-          </span>
-        </header>
-        <div className="flex w-full items-center">
-          <div className="flex-1">
-            <h1 className="text-5xl font-bold">
-              Find & Search Your{" "}
-              <span className="text-[#0360D9] favorite">Favourite</span> Doctor
-            </h1>
-            <small>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem velit
-              viverra amet faucibus.
-            </small>
-            <div className="mt-16">
-              <select>
-                <option>Doctor's Name</option>
-              </select>
-              <select>
-                <option>Location</option>
-              </select>
-              <button>Search</button>
+            <nav className="flex space-x-2 justify-center">
+              <a href="#" className="hover:text-blue-500 hover:underline">
+                Home
+              </a>
+              <a href="#">About</a>
+              <a href="#">Application</a>
+              <a href="#">History</a>
+            </nav>
+            <span className="flex space-x-2">
+              <button className="border border-blue-600 rounded-full p-2 bg-blue-500">
+                Log In
+              </button>
+              <button>Sign Up</button>
+            </span>
+          </header>
+          <div className="flex w-full items-center">
+            <div className="flex-1">
+              <h1 className="text-5xl font-bold">
+                Find & Search Your{" "}
+                <span className="text-[#0360D9] favorite">Favourite</span>{" "}
+                Doctor
+              </h1>
+              <small>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem
+                velit viverra amet faucibus.
+              </small>
+              <div className="mt-16">
+                <select>
+                  <option>Doctor's Name</option>
+                </select>
+                <select>
+                  <option>Location</option>
+                </select>
+                <button>Search</button>
+              </div>
+            </div>
+            <div className="flex-1">
+              <img
+                className="relative w-[20rem] h-auto -bottom-12"
+                src={doctor}
+                alt=""
+              />
             </div>
           </div>
-          <div className="flex-1">
-            <img
-              className="relative w-[20rem] h-auto -bottom-12"
-              src={doctor}
-              alt=""
-            />
-          </div>
         </div>
-      </div>
 
-      {/* Blue section */}
-      <div className="bg-[#0360D9]">
-        <div className="container mx-auto flex w-full py-4">
-          <div className="flex flex-1 items-center justify-between divide-x divide-blue-100">
-            <span className="flex-1 text-center">
-              <p>24/7</p>
-              <small>Online Support</small>
-            </span>
-            <span className="flex-1 text-center">
-              <p>100+</p>
-              <small>Doctors</small>
-            </span>
-            <span className="flex-1 text-center">
-              <p>1M+</p>
-              <small>Active Patients</small>
-            </span>
+        {/* Blue section */}
+        <div className="bg-[#0360D9]">
+          <div className="w-[1440px] mx-auto flex w-full py-4">
+            <div className="flex flex-1 items-center justify-between divide-x divide-blue-100">
+              <span className="flex-1 text-center">
+                <p>24/7</p>
+                <small>Online Support</small>
+              </span>
+              <span className="flex-1 text-center">
+                <p>100+</p>
+                <small>Doctors</small>
+              </span>
+              <span className="flex-1 text-center">
+                <p>1M+</p>
+                <small>Active Patients</small>
+              </span>
+            </div>
+            <div className="flex-1"></div>
           </div>
-          <div className="flex-1"></div>
         </div>
-      </div>
-    </section>
+      </section>
+
+      {/* Second section */}
+      <section className="w-[1440px] mx-auto ibm-plex">
+        {/* Our Consulting Specialists */}
+        <div className="pb-12">
+          <h1 className="text-[#2E2E2E] text-3xl font-bold pt-12 pb-8">
+            Our Consulting Specialists
+          </h1>
+          <div className="flex w-full justify-center gap-4">
+            <div className="flex-1 border border-[#021526] rounded-xl p-4">
+              <img className="bg-[#E6F5FC]" src={covid19} alt="" />
+              <p>Covid-19 Test</p>
+              <small>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem
+                velit viverra amet faucibus.
+              </small>
+            </div>
+            <div className="flex-1 border border-[#021526] rounded-xl p-4">
+              <img src={heartLungs} alt="" />
+              <p>Heart Lungs</p>
+              <small>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem
+                velit viverra amet faucibus.
+              </small>
+            </div>
+            <div className="flex-1 border border-[#021526] rounded-xl p-4">
+              <img src={supplement} alt="" />
+              <p>Supplement</p>
+              <small>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem
+                velit viverra amet faucibus.
+              </small>
+            </div>
+            <div className="flex-1 border border-[#021526] rounded-xl p-4">
+              <img src={mentalHealth} alt="" />
+              <p>Mental Health</p>
+              <small>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem
+                velit viverra amet faucibus.
+              </small>
+            </div>
+          </div>
+        </div>
+
+        {/* Why You Choose Us? */}
+        <div className="flex w-full items-center pb-12">
+          <div className="flex-1">
+            <img src={whyYouChooseUs} alt="" />
+          </div>
+          <div className="flex-1">
+            <h1 className="text-[#2E2E2E] text-3xl font-bold pt-12 pb-8">
+              Why You Choose Us?
+            </h1>
+            <div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <a href="#">Learn More</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Third section */}
+      <section className="bg-[#E1EEFF] ibm-plex">
+        <div className="flex w-[1440px] mx-auto items-center py-12">
+          {/* What Our Member's Saying About Us */}
+          <div className="flex-1">
+            <h1 className="text-5xl font-bold py-4">
+              What <span className="text-[#0360D9] favorite">Our Member's</span>{" "}
+              Saying About Us
+            </h1>
+            <p className="text-lg py-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem velit
+              viverra amet faucibus.
+            </p>
+            <div className="flex items-center">
+              <div className="overlap-images flex-1">
+                <img src={user1} alt="" />
+                <img src={user2} alt="" />
+                <img src={user3} alt="" />
+                <img src={user4} alt="" />
+                <img src={user5} alt="" />
+                <img src={user6} alt="" />
+              </div>
+              <small className="flex-1">100+ Reviews</small>
+            </div>
+          </div>
+
+          {/* Reviews */}
+          <div className="flex-1 bg-white rounded-xl p-4">
+            <div className="flex justify-between">
+              <div className="flex flex-1 items-center ">
+                <img src={user5} alt="" />
+                <div>
+                  <p>Jane Cooper</p>
+                  <small>12/4/17</small>
+                </div>
+              </div>
+              <div className="flex flex-1">
+                <img src={reviewStar} alt="" />
+                <img src={reviewStar} alt="" />
+                <img src={reviewStar} alt="" />
+                <img src={reviewStar} alt="" />
+                <img src={reviewStar} alt="" />
+              </div>
+            </div>
+            <div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem
+                velit viverra amet faucibus. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Sem velit viverra amet faucibus.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sem
+                velit viverra amet faucibus.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
